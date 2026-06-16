b510d36967c17001b46bebe39e1b8e4393558f95
jalb777 <jakealbert70@gmail.com>
2026-06-16 13:56:30 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -502,7 +502,7 @@ if st.session_state.logged_in:
             date = st.date_input("Test Date")
             st.selectbox("Workout Type",["LT1", "LT2", "VO2"])
             pace = st.text_input("Pace (e.g., 6:30)", "6:00")
-            hr = st.number_input("Heart Rate (BPM)", min_value=40, max_value=220, step=1)
+            hr = st.number_input("Average Rep Heart Rate (BPM)", min_value=40, max_value=220, step=1)
             lactate = st.number_input("Lactate (mmol/L)", min_value=0.0, step=0.1)
             submitted = st.form_submit_button("Save Data Point")
             if submitted:
